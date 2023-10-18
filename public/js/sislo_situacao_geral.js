8202b53680f0e71bf9fa499f89ea53651b385a15
vitordorneless <vitordorneles@hotmail.com>
2023-10-18 17:13:52 -0300

    correção order da situação geral

--- public/js/sislo_situacao_geral.js
+++ public/js/sislo_situacao_geral.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
                 success: function (datas) {
                     clearErrors();
                     $("#antes").empty();
-                    
+
                     $("#comissao_jogos").html(datas.comissao_jogos);
                     $("#comissao_bolao").html(datas.comissao_bolao);
                     $("#comissao_jogos_silce").html(datas.comissao_jogos_silce);
@@ -42,24 +42,24 @@ $(document).ready(function () {
                     $("#total_todos_jogos").html(datas.total_todos_jogos);
                     $("#total_todos_nao_jogos").html(datas.total_todos_nao_jogos);
                     $("#total_todos_deveres").html(datas.total_todos_deveres);
-                    $("#total_todos_situacao").html(datas.total_todos_situacao);                    
-                    
-                    $("#total_meta_nao_jogos").html(datas.total_meta_nao_jogos);                    
-                    $("#total_meta_jogos").html(datas.total_meta_jogos);                    
-                    $("#total_todas_metas").html(datas.total_todas_metas);                    
-                    $("#total_metas_atingido").html(datas.total_metas_atingido);                    
-                    
+                    $("#total_todos_situacao").html(datas.total_todos_situacao);
+
+                    $("#total_meta_nao_jogos").html(datas.total_meta_nao_jogos);
+                    $("#total_meta_jogos").html(datas.total_meta_jogos);
+                    $("#total_todas_metas").html(datas.total_todas_metas);
+                    $("#total_metas_atingido").html(datas.total_metas_atingido);
+
                     //$('html,body').animate({scrollTop: document.body.scrollHeight}, "fast");
-                    
+
                     $("#table_sislo_situacao_jogos_geral").DataTable({
                         oLanguage: DATATABLE_PTBR,
                         destroy: true,
                         searching: false,
                         ordering: false,
-                        order: [[4,'desc']],
+                        order: [[4, 'desc']],
                         data: datas.data_jogos
                     });
-                    
+
                     $("#table_sislo_contas_pagar").DataTable({
                         oLanguage: DATATABLE_PTBR,
                         destroy: true,
@@ -67,49 +67,48 @@ $(document).ready(function () {
                         ordering: false,
                         data: datas.data_contas_pagar
                     });
-                    
+
                     $("#table_sislo_salarios").DataTable({
                         oLanguage: DATATABLE_PTBR,
                         destroy: true,
                         searching: false,
                         ordering: false,
-                        order: [[3,'desc']],
+                        order: [[3, 'desc']],
                         data: datas.data_salarios
                     });
-                    
+
                     $("#table_sislo_caixas").DataTable({
                         oLanguage: DATATABLE_PTBR,
                         destroy: true,
                         searching: false,
                         ordering: false,
-                        order: [[2,'desc']],
+                        order: [[2, 'desc']],
                         data: datas.data_caixas
                     });
-                    
+
                     $("#table_sislo_nao_jogos").DataTable({
                         oLanguage: DATATABLE_PTBR,
                         destroy: true,
                         searching: false,
                         ordering: false,
-                        order: [[3,'desc']],
+                        order: [[3, 'desc']],
                         data: datas.data_nao_jogos
                     });
-                    
+
                     $("#table_sislo_situacao_bolao_geral").DataTable({
                         oLanguage: DATATABLE_PTBR,
                         destroy: true,
                         searching: false,
-                        ordering: false,
-                        order: [[4,'desc']],
+                        order: [[4, 'desc']],
                         data: datas.data_bolao
                     });
-                    
+
                     $("#table_sislo_situacao_jogos_silce_geral").DataTable({
                         oLanguage: DATATABLE_PTBR,
                         destroy: true,
                         searching: false,
                         ordering: false,
-                        order: [[3,'desc']],
+                        order: [[3, 'desc']],
                         data: datas.data_silce
                     });
 
@@ -118,25 +117,25 @@ $(document).ready(function () {
                         destroy: true,
                         searching: false,
                         ordering: false,
-                        order: [[3,'desc']],
+                        order: [[3, 'desc']],
                         data: datas.data_ibc
                     });
-                    
+
                     $("#table_sislo_bilhete_federal").DataTable({
                         oLanguage: DATATABLE_PTBR,
                         destroy: true,
                         searching: false,
                         ordering: false,
-                        order: [[3,'desc']],
+                        order: [[3, 'desc']],
                         data: datas.data_bilhete_federal
                     });
-                    
+
                     $("#table_sislo_premios_pagos").DataTable({
                         oLanguage: DATATABLE_PTBR,
                         destroy: true,
                         searching: false,
                         ordering: false,
-                        order: [[3,'desc']],
+                        order: [[3, 'desc']],
                         data: datas.data_premios_pagos
                     });
                 }
